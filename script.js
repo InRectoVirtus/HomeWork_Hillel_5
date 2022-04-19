abe62fd00d1a70c0b8ffbb640e8002c330082df1
InRectoVirtus <inrectovirtus.8.32@gmail.com>
2022-04-19 19:00:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let sumFirstArray = 0;
+let sumSecondArray = 0;
+
+function arrayBigger (firstArray, secondArray) {
+    sumFirstArray = 0;
+    sumSecondArray = 0;
+    if(((firstArray.every((elem) => typeof elem == 'number')) == true) && ((secondArray.every((elem) => typeof elem == 'number')) == true)) {
+        
+        for(i = 0; i < firstArray.length; i++) {
+            sumFirstArray += firstArray[i];
+        };
+
+        for(j = 0; j < secondArray.length; j++) {
+            sumSecondArray += secondArray[j];
+        };
+
+        if(sumFirstArray > sumSecondArray) {
+            return firstArray;
+
+        } else if (sumFirstArray < sumSecondArray) {
+            return secondArray;
+
+        }; 
+
+    } else {
+        return `your array has another type element`;
+    };
+};
+
