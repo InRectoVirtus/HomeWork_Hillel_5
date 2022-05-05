5829c6df21f79ee03c8705bf3d3ac715cbc1ed18
InRectoVirtus <82666923+InRectoVirtus@users.noreply.github.com>
2022-05-05 17:54:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 function arrayBigger (firstArray, secondArray) {
+    
     let sumFirstArray = 0;
     let sumSecondArray = 0;
-     
-        for(let i = 0; i < firstArray.length; i++) {
+        
+    for(i = 0; i < firstArray.length; i++) {
+        if(typeof firstArray[i] == `number`) {
             sumFirstArray += firstArray[i];
-        };
+        } else {
+            continue;
+        }
+    };
 
-        for(let j = 0; j < secondArray.length; j++) {
-            sumSecondArray += secondArray[j];
-        };
+    for(j = 0; j < secondArray.length; j++) {
+        if(typeof secondArray[j] == `number`) {
+        sumSecondArray += secondArray[j];
+        } else {
+            continue;
+        }
+    };
 
-        if(sumFirstArray > sumSecondArray) {
-            return firstArray;
+    if (sumFirstArray > sumSecondArray){
+        return firstArray;
+    } else {
+        return secondArray;
+    };
 
-        } else if (sumFirstArray < sumSecondArray) {
-            return secondArray;
-
-        }; 
 };
 
-arrayBigger ([2,1,3], [2,1,2]);
-arrayBigger ([2,1,3], [2,5,3]);
+console.log(arrayBigger([2,1,3], [2,1,2]));
+console.log(arrayBigger([2,1,3], [2,5,3]));
 
+console.log(arrayBigger([1,2,3,false, 4, 'hello',5, {x: 10},6], [1,2,3,false, 4, 'hello',5, {x: 10},30]));
+console.log(arrayBigger([1,2,3,false, 4, 'hello',5, {x: 10},41], [1,2,3,false, 4, 'hello',5, {x: 10},30]));
